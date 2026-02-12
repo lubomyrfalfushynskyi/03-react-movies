2ac3a892a65f424bfa87f42de04fdeed4c6ebaf1
Admin <lubomyrfalfushinskyi@gmail.com>
2026-02-12 23:35:29 +0200

    ver2

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import './index.css'
 import App from './App.tsx'
-import 'modern-normalize';
+import 'modern-normalize/modern-normalize.css';
 
 ReactDom.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
